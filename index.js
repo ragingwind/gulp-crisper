2f0d9384b3002aa9b0d8072c7ee6680dd30417d4
ragingwind <ragingwind@gmail.com>
2015-05-04 10:29:55 +0900

    Fix intent

--- index.js
+++ index.js
@@ -5,36 +5,36 @@ var through = require('through2');
 var crisper = require('crisper');
 
 function splitFile(file, filename, contents) {
-	return new gutil.File({
-		cwd: file.cwd,
-		base: file.base,
-		path: path.join(file.base, filename),
-		contents: new Buffer(contents)
-	});
+  return new gutil.File({
+    cwd: file.cwd,
+    base: file.base,
+    path: path.join(file.base, filename),
+    contents: new Buffer(contents)
+  });
 }
 
 function getFilename(filepath) {
-	var basename = path.basename(filepath, path.extname(filepath));
-	return {
-		html: basename + '.html',
-		js: basename + '.js'
-	};
+  var basename = path.basename(filepath, path.extname(filepath));
+  return {
+    html: basename + '.html',
+    js: basename + '.js'
+  };
 }
 
 module.exports = function () {
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+    	cb(null, file);
+    	return;
+    }
 
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-crisper', 'Streaming not supported'));
-			return;
-		}
+    if (file.isStream()) {
+    	cb(new gutil.PluginError('gulp-crisper', 'Streaming not supported'));
+    	return;
+    }
 
-		var splitfile = getFilename(file.path)
-		var split = crisper.split(file.contents.toString(), splitfile.js);
+    var splitfile = getFilename(file.path)
+    var split = crisper.split(file.contents.toString(), splitfile.js);
     var stream = this;
 
     Object.keys(split).forEach(function(type) {
@@ -43,6 +43,6 @@ module.exports = function () {
       }
     });
 
-		cb();
-	});
+    cb();
+  });
 };
