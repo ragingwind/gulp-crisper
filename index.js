e536d300f28bb8e20adee316c3024a81fd571fd9
Lulasz Szymanski <lukasz.szymansky@gmail.com>
2015-06-26 16:21:24 +0200

    fix few issues

--- index.js
+++ index.js
@@ -5,41 +5,50 @@ var through = require('through2');
 var crisper = require('crisper');
 
 function splitFile(file, filename, contents) {
-    return new gutil.File({
-        cwd: file.cwd,
-        base: file.base,
-        path: path.join(file.base, filename),
-        contents: new Buffer(contents)
-    });
+  return new gutil.File({
+    cwd: file.cwd,
+    base: file.base,
+    path: path.join(file.base, filename),
+    contents: new Buffer(contents)
+  });
 }
 
 function getFilename(file) {
     var htmlPath = path.relative(file.base, file.path);
     var jsPath = gutil.replaceExtension(htmlPath, '.js');
-    return { html: htmlPath, js: jsPath };
+    var jsFilename = path.basename(jsPath);
+    return { 
+        html: htmlPath, 
+        js: { 
+            path: jsPath, 
+            filename: jsFilename
+        }
+    };
 }
 
 module.exports = function () {
-    return through.obj(function (file, enc, cb) {
-        if (file.isNull()) {
-            cb(null, file);
-            return;
-        }
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+    	cb(null, file);
+    	return;
+    }
 
-        if (file.isStream()) {
-            cb(new gutil.PluginError('gulp-crisper', 'Streaming not supported'));
-            return;
-        }
-        var splitfile = getFilename(file);
-        var split = crisper.split(file.contents.toString(), splitfile.js);
-        var stream = this;
+    if (file.isStream()) {
+    	cb(new gutil.PluginError('gulp-crisper', 'Streaming not supported'));
+    	return;
+    }
+    var splitfile = getFilename(file);
+    var htmlContent = file.contents.toString();
+    var split = crisper.split(htmlContent, splitfile.js.filename);
+    var stream = this;
 
-        Object.keys(split).forEach(function(type) {
-          if (split[type]) {
-            stream.push(splitFile(file, splitfile[type], split[type]));
-          }
-        });
+    if (split.js.length < 1) {
+        stream.push(splitFile(file, splitfile.html, htmlContent));
+    } else {
+        stream.push(splitFile(file, splitfile.html, split.html));
+        stream.push(splitFile(file, splitfile.js.path, split.js));
+    }
 
-        cb();
-    });
+    cb();
+  });
 };
