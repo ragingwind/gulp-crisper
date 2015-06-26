374c4852bc1d8ec3e1775817ad631a9b5b6cf53d
Lulasz Szymanski <lukasz.szymansky@gmail.com>
2015-06-26 15:16:25 +0200

    hopfix; fix relative path issue

--- index.js
+++ index.js
@@ -5,44 +5,41 @@ var through = require('through2');
 var crisper = require('crisper');
 
 function splitFile(file, filename, contents) {
-  return new gutil.File({
-    cwd: file.cwd,
-    base: file.base,
-    path: path.join(file.base, filename),
-    contents: new Buffer(contents)
-  });
+    return new gutil.File({
+        cwd: file.cwd,
+        base: file.base,
+        path: path.join(file.base, filename),
+        contents: new Buffer(contents)
+    });
 }
 
-function getFilename(filepath) {
-  var basename = path.basename(filepath, path.extname(filepath));
-  return {
-    html: basename + '.html',
-    js: basename + '.js'
-  };
+function getFilename(file) {
+    var htmlPath = path.relative(file.base, file.path);
+    var jsPath = gutil.replaceExtension(htmlPath, '.js');
+    return { html: htmlPath, js: jsPath };
 }
 
 module.exports = function () {
-  return through.obj(function (file, enc, cb) {
-    if (file.isNull()) {
-    	cb(null, file);
-    	return;
-    }
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
 
-    if (file.isStream()) {
-    	cb(new gutil.PluginError('gulp-crisper', 'Streaming not supported'));
-    	return;
-    }
+        if (file.isStream()) {
+            cb(new gutil.PluginError('gulp-crisper', 'Streaming not supported'));
+            return;
+        }
+        var splitfile = getFilename(file);
+        var split = crisper.split(file.contents.toString(), splitfile.js);
+        var stream = this;
 
-    var splitfile = getFilename(file.path)
-    var split = crisper.split(file.contents.toString(), splitfile.js);
-    var stream = this;
+        Object.keys(split).forEach(function(type) {
+          if (split[type]) {
+            stream.push(splitFile(file, splitfile[type], split[type]));
+          }
+        });
 
-    Object.keys(split).forEach(function(type) {
-      if (split[type]) {
-        stream.push(splitFile(file, splitfile[type], split[type]));
-      }
+        cb();
     });
-
-    cb();
-  });
 };
